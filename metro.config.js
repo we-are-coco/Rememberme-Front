d7f2647c7b6b566603d179942ef975f60c1b84d5
MyeongGil Jeong <itsstylemk@gmail.com>
2025-02-20 16:56:18 +0900

    fix: eas.json env 명칭 수정

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,12 @@
-const {getDefaultConfig} = require("expo/metro-config");
-const {withNativeWind} = require('nativewind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
 config.transformer = {
-    ...config.transformer,
-    _expoRelativeProjectRoot: __dirname
-}
+  ...config.transformer,
+  _expoRelativeProjectRoot: __dirname,
+};
 
 // noinspection JSCheckFunctionSignatures
-module.exports = withNativeWind(config, {input: './global.css'})
+module.exports = withNativeWind(config, { input: "./global.css" });
